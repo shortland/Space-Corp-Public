8a230c3f0c543484510660415d0ce2735befa8c3
Ilan Kleiman <ilankleiman@gmail.com>
2017-02-01 19:12:20 -0500

    parsing of some data format
    data can be read as: “no, no, yes, no” or ‘“no”, “yes”, “no”, “yes”’.
    But is written back without quotes. New accounts in DB are written
    with, but after a single read/write they no longer contain quotation
    marks

---
+++ sides/StringArray.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+sub parseStringArray {
+	my @parms = @_;
+
+	my @values = split(',', $parms[0]);
+	foreach my $val (@values) {
+		$val =~ s/["| |']//g; # technically support for legacy code format
+	}
+	if(defined $parms[1]) {
+		return @values[$parms[1]];
+	}
+	else {
+		return @values;
+	}
+}
+
+# received from database
+$s = 'no, yes, yes, no, yes, yes, no, yes, no, yes, yes';
+@arr = parseStringArray($s);
+
+for($i = 0; $i < @arr.length; $i++) {
+	if(@arr[$i] =~ /^yes$/) {
+		print $i . " is yes!\n";
+		print "Settings it to false!\n";
+		
+		@arr[$i] = "no";
+		$scal = join(", ", @arr);
+		print "Success!\n\n";
+	}
+}
+
+print $scal . "\n";
