0ada51f2da96b6f91b05407eae74479fc09d9e47
Ilan Kleiman <ilankleiman@gmail.com>
2017-12-07 13:23:20 -0500

    collaborates with cookieLogin.pl -  use on any login-required files in future.

---
+++ server/Spacecorp/LoggedIn.pm
@@ -0,0 +1,27 @@
+package Spacecorp::LoggedIn;
+
+use strict;
+use warnings;
+
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use DBI;
+use YAML::XS 'LoadFile';
+ 
+use Exporter qw(import);
+use Data::Dumper;
+our @EXPORT_OK = qw(attempt_login);
+
+sub attempt_login {
+	my $cgi = new CGI;
+	if (!defined $cgi->cookie('uCookie') && !defined $cgi->cookie('pCookie')) {
+		return undef;
+	}
+    my $config = LoadFile('../Spacecorp/.config.yaml');
+	my $DBH = DBI->connect("DBI:mysql:database=spacecorp;host=localhost", $config->{sql}{user}, $config->{sql}{pass}, {'RaiseError' => 1});
+    my $sth = $DBH->prepare("SELECT username FROM user WHERE u_cookie = ? AND p_cookie = ?");
+    $sth->execute($cgi->cookie('uCookie'), $cgi->cookie('pCookie')) or return 0;
+    return $sth->fetchrow_hashref();
+}
+
+1;
